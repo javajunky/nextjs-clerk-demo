55a962b844f73fb9aa4c4f03057a48ea32e072b6
Wil <wilchow@gmail.com>
2024-04-08 20:23:10 -0500

    🖼️ Added image 🎨 Styles

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,21 +2,24 @@
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
 import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
 
 function Page() {
   const { user } = useUser();
   console.log(user);
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-24">
-      <div className="fixed top-0 left-0 z-10 items-center justify-end w-full max-w-5xl py-10 font-mono text-sm lg:flex">
+    <main className="flex flex-col items-center justify-center min-h-screen gap-8 p-8 md:p-24">
+      <div className="fixed top-0 left-0 z-10 flex items-center justify-end w-full px-4 py-10 font-mono text-sm md:px-10">
         <div className="flex gap-4">
           <div className="flex items-center justify-center border-2 rounded-full border-emerald-800 dark:border-white size-9 bg-emerald-400">
             {user ? <UserButton afterSignOutUrl="/" /> : null}
           </div>
         </div>
       </div>
-
-      <h1 className="font-bold text-7xl">This is a private page!</h1>
+      <Image src="/dwight-secret.gif" alt="Secret!" width={480} height={270} />
+      <h1 className="text-3xl font-black md:text-7xl">
+        This is a private page!
+      </h1>
     </main>
   );
 }
