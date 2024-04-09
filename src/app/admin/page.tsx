1507ac0f1f20ea8a42c5901e5bb98a91f767efc3
Wil <wilchow@gmail.com>
2024-04-08 20:17:56 -0500

    🎨 Updated styles

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,17 @@
+"use client";
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
+import { useUser } from "@clerk/nextjs";
 
 function Page() {
+  const { user } = useUser();
+  console.log(user);
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-24">
       <div className="fixed top-0 left-0 z-10 items-center justify-end w-full max-w-5xl py-10 font-mono text-sm lg:flex">
         <div className="flex gap-4">
-          <div className="border-2 rounded-full border-emerald-800 dark:border-white">
-            <UserButton afterSignOutUrl="/admin" />
+          <div className="flex items-center justify-center border-2 rounded-full border-emerald-800 dark:border-white size-9 bg-emerald-400">
+            {user ? <UserButton afterSignOutUrl="/" /> : null}
           </div>
         </div>
       </div>
